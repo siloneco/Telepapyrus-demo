2a89794502ea22cbff05eaf563fd4c35fa790055
siloneco <siloneco4646@gmail.com>
2024-01-05 02:49:26 +0900

    refactor: delete sitemap from robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,14 +2,11 @@ import { MetadataRoute } from 'next'
 
 export const dynamic = 'force-static'
 
-const baseUrl = process.env.BASE_URL || ''
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       disallow: ['/'],
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
